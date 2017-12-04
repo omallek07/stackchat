fcfaf059c2a389ef47c4bb8422b017516d8b3365
Kevin <omallek@gmail.com>
2017-12-04 10:42:51 -0600

    monday

--- client/components/NewMessageEntry.js
+++ client/components/NewMessageEntry.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import store, { newMessageFromUser } from '../store';
 
 export default class NewMessageEntry extends Component {
+  constructor() {
+    super();
+    this.state = store.getState();
+  }
 
+  componentDidMount () {
+    this.unsubscribe = store.subscribe(() => this.setState(store.getState()));
+  }
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+  changeHandler (evt) {
+    let value = evt.target.value;
+    const action = newMessageFromUser(value);
+    store.dispatch(action);
+  }
   render () {
+    console.log(this.state.newMessageEntry);
     return (
       <form id="new-message-form">
         <div className="input-group input-group-lg">
@@ -10,7 +27,9 @@ export default class NewMessageEntry extends Component {
             className="form-control"
             type="text"
             name="content"
+            value={this.state.newMessageEntry}
             placeholder="Say something nice..."
+            onChange={this.changeHandler}
           />
           <span className="input-group-btn">
             <button className="btn btn-default" type="submit">Chat!</button>
--- client/store.js
+++ client/store.js
@@ -1,16 +1,20 @@
 import { createStore } from 'redux'
 
 const initialState = {
-  messages: []
+  messages: [],
+  newMessageEntry: '',
 }
 
-const DELETE_MESSAGE = 'DELETE_MESSAGE';
+const NEW_MESSAGE = 'NEW_MESSAGE';
 const GOT_MESSAGES_FROM_SERVER = 'GOT_MESSAGES_FROM_SERVER';
+const GOT_NEW_MESSAGE_FROM_SERVER = 'GOT_NEW_MESSAGE_FROM_SERVER';
 
-
-const deleteMessageAction = () => {
-  return {type: DELETE_MESSAGE}
-};
+export const newMessageFromUser = (message) => {
+  return {
+    type: NEW_MESSAGE,
+    newMessageEntry: message
+  }
+}
 
 export const gotMessagesFromServer = (messages) => {
   return {
@@ -19,16 +23,26 @@ export const gotMessagesFromServer = (messages) => {
     }
 };
 
+export const gotNewMessageFromServer = (message) => {
+  return {
+    type: GOT_NEW_MESSAGE_FROM_SERVER,
+    message: message
+    }
+};
 
 function reducer (state = initialState, action) {
+  let prevState = state;
   switch (action.type) {
     case GOT_MESSAGES_FROM_SERVER:
-       return Object.assign({}, state, { messages: action.messages });
+      return Object.assign({}, state, { messages: action.messages });
+    case NEW_MESSAGE:
+      return Object.assign({}, prevState, {  newMessageEntry: action.newMessageEntry })
+    case GOT_NEW_MESSAGE_FROM_SERVER:
+      return Object.assign({}, prevState, { messages: [...prevState.messages, action.message] });
     default:
-       return state;
+       return prevState;
   }
 }
-
 
 let store = createStore(reducer)
 export default store;
